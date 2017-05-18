f745c898d4af473000976c2c89b161d0ea7c571a
Nathan Smith <nathan@nathansmith.io>
2017-05-18 14:30:26 -0700

    Add fuck

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
 set fish_greeting ""
 
 # Set virtualfish path
-eval (python3 -m virtualfish)
+eval (python -m virtualfish)
+
+#set PATH $HOME/.rbenv/bin $PATH
+#set PATH $HOME/.rbenv/shims $PATH
+#rbenv rehash >/dev/null ^&1
 
 # Load aliases
 source ~/.config/fish/aliases.fish
