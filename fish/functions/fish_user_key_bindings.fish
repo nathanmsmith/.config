69326fc9f15b1b720e526323620f042219ab38e5
Nathan Smith <nathan@nathansmith.io>
2019-02-02 18:54:22 -0500

    better vim mode!

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,4 @@
 # Custom fish keybindings
 function fish_user_key_bindings -d "Custom keybindings"
   fish_vi_key_bindings
-
-  bind -M default h backward-char
-  bind -M default s forward-char
-  bind n up-or-search
-  bind t down-or-search
 end
