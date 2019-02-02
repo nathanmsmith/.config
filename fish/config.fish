71d62d731acb604f7d85b2fac1bb8c840866f491
Nathan Smith <nathan@nathansmith.io>
2019-02-02 18:54:15 -0500

    Some path stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 set fish_greeting ""
 
-# TODO: use -U option
-
 # Set vi bindings
 set fish_key_bindings fish_user_key_bindings
 
@@ -9,14 +7,27 @@ set fish_key_bindings fish_user_key_bindings
 source ~/.config/fish/aliases.fish
 
 # More secure homebrew settings
-set -x HOMEBREW_NO_INSECURE_REDIRECT 1
-set -x HOMEBREW_CASK_OPTS --require-sh
+set -gx HOMEBREW_NO_INSECURE_REDIRECT 1
+set -gx HOMEBREW_CASK_OPTS --require-sh
 
 # Use GNU coreutils instead of macOS versions
-# set -x /usr/local/opt/coreutils/libexec/gnubin:$PATH
+set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 
+# JavaScript specific settings
+# Set up nodenv
+status --is-interactive; and source (nodenv init -|psub)
+
+# Ruby specific settings
+# Rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
+# Go specific settings
 # GOPATH
-set -x GOPATH $HOME/Developer/go
+set -gx GOPATH $HOME/Developer/go
 
+# Add locally compiled go programs to bin
+set -gx PATH $PATH $GOPATH/bin
+
+# OCaml specific settings
 # Opam configuration
 source /Users/nathan/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
