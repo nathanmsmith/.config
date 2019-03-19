80f49637729c5c5bd35dba19c642328fa83858f6
Nathan Smith <nathan@nathansmith.io>
2019-03-18 20:26:30 -0400

    Merge stuff in

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,5 @@
 alias tower "gittower ."
 alias vimtutor "code ~/.config/tutor.txt"
 alias typora "open -a typora"
+alias g "git"
 # alias macchanger 'openssl rand -hex 6 | sed "s/\(..\)/\1:/g; s/.$//" | xargs sudo ifconfig en0 ether'
