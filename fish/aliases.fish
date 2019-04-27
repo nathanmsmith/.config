1c20eb36b45940e3a97fad220bedb2ee687d7fd0
Nathan Smith <nathan@nathansmith.io>
2019-04-27 10:18:11 -0400

    stuff from datadog

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,13 +1,23 @@
 alias tower "gittower ."
 alias typora "open -a typora"
 # alias macchanger 'openssl rand -hex 6 | sed "s/\(..\)/\1:/g; s/.$//" | xargs sudo ifconfig en0 ether'
+alias config "cd $XDG_CONFIG_HOME; and $EDITOR $XDG_CONFIG_HOME"
+alias wiki "cd ~/wiki; and $EDITOR ~/wiki"
 
 # Abbreviations
 # See https://github.com/fish-shell/fish-shell/issues/1976#issuecomment-168698602
 # if not set -q abbr_initialized
 #   set -U abbr_initialized
+
+  abbr --add cfg 'config'
+  abbr --add w 'wiki'
+
+  abbr --add o 'open .'
+
+  # Git
   abbr --add g 'git'
   abbr --add ga 'git add'
+  abbr --add gi 'git ignore'
   abbr --add gad 'git add .'
   abbr --add ga. 'git add .'
   abbr --add gst 'git status'
@@ -15,13 +25,18 @@ alias typora "open -a typora"
   abbr --add gco 'git checkout'
   abbr --add gcm 'git commit -m'
   abbr --add gcma 'git commit -ma'
+  abbr --add gbd 'git branch -d'
+  abbr --add gbD 'git branch -D'
   abbr --add gp 'git push'
+  abbr --add gpu 'git push -u origin'
   abbr --add gpf 'git push --force-with-lease'
   abbr --add gpl 'git pull'
   abbr --add gr 'git rebase'
   abbr --add gri 'git rebase --interactive'
   abbr --add gst 'git status'
   abbr --add gd 'git diff'
+
+  # Yarn
   abbr --add yn 'yarn'
   abbr --add yns 'yarn start'
 # end
---
+++ fish/functions/gi.fish
@@ -0,0 +1,3 @@
+function gi
+	curl -sL https://www.gitignore.io/api/$argv
+end
