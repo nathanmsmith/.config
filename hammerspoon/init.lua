3b4cbc4dcc9b69b09282ff1fd7129b3dd14a5e88
Nathan Smith <nathan@nathansmith.io>
2020-05-23 17:01:25 -0700

    Update caffinate shortcut

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,13 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.loadSpoon("Caffeine")
-spoon.Caffeine:start()
+local caffeine = hs.loadSpoon("Caffeine")
+caffeine:bindHotkeys({toggle = {{"ctrl", "alt", "cmd"}, "a"}})
+caffeine:start()
+
+hs.inspect(spoon.Caffeine)
+
 
 
 -- Defeat paste blocking
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
-
-
--- TeX to Unicode shortcuts
