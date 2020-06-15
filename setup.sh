4793023b73acabc1295fcdd271f6dd57d3e3cbfd
Nathan Smith <nathan@nathansmith.io>
2020-06-15 12:38:52 -0700

    Add sudo check

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install Command Line Tools
 echo "Installing macOS Command Line Tools..."
 xcode-select --install
