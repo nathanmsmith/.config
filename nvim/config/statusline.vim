4d167b9316c1fd0a0968721774b5f1e8cad819dd
Nathan Smith <nathan@nathansmith.io>
2020-08-13 08:34:28 -0700

    Roll own lsp status line

--- nvim/config/statusline.vim
+++ nvim/config/statusline.vim
@@ -9,14 +9,29 @@ endfunction
 " Statusline
 function! LspStatus() abort
   if luaeval('#vim.lsp.buf_get_clients() > 0')
-    return luaeval("require('lsp-status').status()")
+    return 'LSP'
   endif
   return ''
 endfunction
 
 function TreeSitter() abort
-  return luaeval("require('nvim-treesitter').statusline(20)")
+  return luaeval("require('nvim-treesitter').statusline(20) or ''")
 endfunction
+
+function! LinterStatus() abort
+  let l:counts = ale#statusline#Count(bufnr(''))
+
+  let l:all_errors = l:counts.error + l:counts.style_error
+  let l:all_non_errors = l:counts.total - l:all_errors
+
+  return l:counts.total == 0 ? 'OK' : printf(
+        \   '%dW %dE',
+        \   all_non_errors,
+        \   all_errors
+        \)
+endfunction
+
+set statusline=%{LinterStatus()}
 
 set laststatus=2
 set statusline=
@@ -25,7 +40,7 @@ set statusline+=%m
 set statusline+=\ %{FugitiveStatusline()}
 set statusline+=\ %{LspStatus()}
 set statusline+=\ %{gutentags#statusline()}
-set statusline+=\ %{TreeSitter()}
 set statusline+=%=
-set statusline+=%{SyntaxItem()}
+set statusline+=%{TreeSitter()}
+set statusline+=\ %{SyntaxItem()}
 set statusline+=\ %y
--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,4 +1,4 @@
-local lsp_status = require('lsp-status')
+-- local lsp_status = require('lsp-status')
 local diagnostic = require('diagnostic')
 local completion = require('completion')
 local nvim_lsp = require('nvim_lsp')
@@ -6,60 +6,60 @@ local configs = require('nvim_lsp/configs')
 local util = require('nvim_lsp/util')
 
 local on_attach = function(client, bufnr)
-  lsp_status.on_attach(client, bufnr)
+  -- lsp_status.on_attach(client, bufnr)
   diagnostic.on_attach(client, bufnr)
   completion.on_attach(client, bufnr)
 
   -- Keybindings for LSPs
-  -- vim.fn.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
-  -- vim.fn.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
+  vim.fn.nvim_set_keymap("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
 end
-lsp_status.register_progress()
-lsp_status.config({
-  status_symbol = '',
-  indicator_errors = 'e',
-  indicator_warnings = 'w',
-  indicator_info = 'i',
-  indicator_hint = 'h',
-  indicator_ok = 'ok',
-})
+-- lsp_status.register_progress()
+-- lsp_status.config({
+--   status_symbol = '',
+--   indicator_errors = 'e',
+--   indicator_warnings = 'w',
+--   indicator_info = 'i',
+--   indicator_hint = 'h',
+--   indicator_ok = 'ok',
+-- })
 
 nvim_lsp.clangd.setup({
-  callbacks = lsp_status.extensions.clangd.setup(),
+  -- callbacks = lsp_status.extensions.clangd.setup(),
   init_options = {
     clangdFileStatus = true
   },
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 })
 nvim_lsp.pyls.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.sumneko_lua.setup{
   on_attach = on_attach,
 }
 nvim_lsp.vimls.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.tsserver.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.html.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.cssls.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities,
+  -- capabilities = lsp_status.capabilities,
   settings = {
     css = {
       validate = false
@@ -103,17 +103,17 @@ configs[server_name] = {
 }
 nvim_lsp.svelte_lsp.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.rust_analyzer.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.elixirls.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
 nvim_lsp.gopls.setup{
   on_attach = on_attach,
-  capabilities = lsp_status.capabilities
+  -- capabilities = lsp_status.capabilities
 }
