48db9ac88ce5719a493ecdc4bc9087f51ee6737e
Nathan Smith <nathan@nathansmith.io>
2020-08-22 22:45:11 -0700

    Switch browser to Safari

--- hammerspoon/urls.lua
+++ hammerspoon/urls.lua
@@ -1,10 +1,10 @@
 -- URL Dispatcher
 hs.loadSpoon("URLDispatcher")
 local Zoom = "us.zoom.xos"
-local Chrome = "com.google.Chrome"
+local Browser = "com.apple.SafariTechnologyPreview"
 spoon.URLDispatcher.url_patterns = {
   {"https?://zoom.us/j/", Zoom},
   {"https?://%w+.zoom.us/j/", Zoom}
 }
-spoon.URLDispatcher.default_handler = Chrome
+spoon.URLDispatcher.default_handler = Browser
 spoon.URLDispatcher:start()
