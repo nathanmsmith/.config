9032d91c4b60674532368ace7274c992c514e925
Nathan Smith <nathan@nathansmith.io>
2020-10-07 23:04:10 -0700

    Fix bugs in poke

--- fish/functions/poke.fish
+++ fish/functions/poke.fish
@@ -1,5 +1,4 @@
-function poke --description 'Touch, but it makes directories'
-  set -l filename basename $argv
-  set -l directoryname dirname $argv
-  mkdir -p $directoryname && touch filename
+function poke --description 'Touch, but it makes directories too'
+  set -l dirpath (dirname $argv)
+  mkdir -p $dirpath && touch $argv
 end
