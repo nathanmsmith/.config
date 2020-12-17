bfa81a97c48c07b6ad4be5d95196badb0e1c84f3
Nathan Smith <nathan@nathansmith.io>
2020-12-16 19:58:13 -0800

    Clean up

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,11 +3,6 @@ hs.window.animationDuration = 0
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-Broswer = "com.apple.Safari"
-hs.hotkey.bind({"ctrl", "cmd"}, "b", function()
-  hs.application.launchOrFocusByBundleID(Browser)
-end)
-
 -- Variable names, no they aren't garbage collected
 b = require('bluetooth')
 c = require('caffeine')
