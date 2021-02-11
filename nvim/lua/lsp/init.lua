588157fd050495befef0f342aebba174c13fd289
Nathan Smith <nathan@nathansmith.io>
2021-02-10 21:04:02 -0800

    Explicitly require nvim-ale-diagnostic

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,5 @@
 local lspconfig = require('lspconfig')
--- local configs = require('lspconfig/configs')
--- local util = require('lspconfig/util')
+require("nvim-ale-diagnostic")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
