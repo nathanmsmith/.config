2eea6c9191f4766643a14591dca499f14458d8ef
Nathan Smith <nathan@nathansmith.io>
2021-04-20 14:05:36 -0700

    Update to_pdf to use xelatex

--- fish/functions/to_pdf.fish
+++ fish/functions/to_pdf.fish
@@ -4,6 +4,6 @@ function to_pdf --description "Take a file and convert it to a PDF"
     set file_split (string split . $file)
     set file_extension $file_split[2]
     set file_name $file_split[1]
-    /usr/local/bin/pandoc $file -f $file_extension -t pdf -o "$file_name.pdf"
+    /usr/local/bin/pandoc $file -f $file_extension -t pdf -o "$file_name.pdf" --pdf-engine=xelatex
   end
 end
