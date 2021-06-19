84db0f1987c059cd18fb9a2c0b79d77204458664
Nathan Smith <nathan@nathansmith.io>
2021-06-18 22:56:47 -0700

    Packer updates

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,21 @@
--- local execute = vim.api.nvim_command
--- local fn = vim.fn
+-- Bootstrap Packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
 
--- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
--- if fn.empty(fn.glob(install_path)) > 0 then
---   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
---   execute 'packadd packer.nvim'
--- end
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
 
+-- Run PackerCompile on save
+vim.api.nvim_exec([[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost plugins.lua PackerCompile
+  augroup end
+]], false)
 
 return require('packer').startup(function()
   -- Packer can manage itself
