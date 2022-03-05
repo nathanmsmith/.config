cc6fb6399c175a19fc16d060bd8cb0b588dad63c
Nathan Smith <nathan@nathansmith.io>
2022-03-05 11:02:56 -0800

    Switch to luasnip

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,11 +1,8 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
-vim.g.UltiSnipsExpandTrigger = "<Plug>(ultisnips_expand)"
-vim.g.UltiSnipsJumpForwardTrigger = "<Plug>(ultisnips_jump_forward)"
-vim.g.UltiSnipsJumpBackwardTrigger = "<Plug>(ultisnips_jump_backward)"
-vim.g.UltiSnipsListSnippets = "<c-x><c-s>"
-vim.g.UltiSnipsRemoveSelectModeMappings = 0
+require("luasnip.loaders.from_snipmate").load()
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -27,73 +24,40 @@ cmp.setup({
       menu = {
         buffer = "[Buffer]",
         nvim_lsp = "[LSP]",
-        ultisnips = "[Snippet]",
+        nvim_lua = "[Lua]",
+        luasnip = "[Snippet]",
         path = "[Path]",
       },
     }),
   },
   snippet = {
     expand = function(args)
-      vim.fn["UltiSnips#Anon"](args.body)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
   mapping = {
-    ["<Tab>"] = cmp.mapping({
-      -- c = function()
-      --   if cmp.visible() then
-      --     cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-      --   else
-      --     cmp.complete()
-      --   end
-      -- end,
-      i = function(fallback)
-        if cmp.get_selected_entry() == nil and vim.fn["UltiSnips#CanExpandSnippet"]() == 1 then
-          vim.api.nvim_feedkeys(t("<C-R>=UltiSnips#ExpandSnippet()<CR>"), "n", true)
-        elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-          vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-        elseif cmp.visible() then
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end,
-      s = function(fallback)
-        if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-          vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), "m", true)
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end,
-    }),
-    ["<S-Tab>"] = cmp.mapping({
-      -- c = function()
-      --   if cmp.visible() then
-      --     cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-      --   else
-      --     cmp.complete()
-      --   end
-      -- end,
-      i = function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-        elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-          return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), "m", true)
-        else
-          fallback()
-        end
-      end,
-      s = function(fallback)
-        if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-          return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), "m", true)
-        else
-          fallback()
-        end
-      end,
-    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
     ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { "i" }),
     ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { "i" }),
     ["<C-n>"] = cmp.mapping({
@@ -145,10 +109,10 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
+    { name = "nvim_lua" },
     { name = "ultisnips" },
-    -- { name = "path" },
-  }, {
-    { name = "buffer" },
+    { name = "path" },
+    { name = "buffer", keyword_length = 5 },
   }),
 })
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -62,14 +62,14 @@ return require("packer").startup(function(use)
   use("hrsh7th/cmp-nvim-lsp")
   use("hrsh7th/cmp-buffer")
   use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-nvim-lua")
   use("hrsh7th/cmp-cmdline")
-  use("quangnguyen30192/cmp-nvim-ultisnips")
+  use("saadparwaiz1/cmp_luasnip")
   use("hrsh7th/nvim-cmp")
   use("onsails/lspkind-nvim")
-  -- use("ervandew/supertab")
 
   -- Snippets
-  use("SirVer/ultisnips")
+  use("L3MON4D3/LuaSnip")
 
   -- Tim Pope Extensions
   use("tpope/vim-eunuch")
