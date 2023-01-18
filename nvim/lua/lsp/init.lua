7e7effa44f8ecfe314961995ac4c805c1f8c5c4b
Nathan Smith <nms@stripe.com>
2023-01-18 13:07:32 -0800

    wip

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,12 @@
+local helpers = require("custom-helpers")
 local servers = require("lsp.servers")
 
 require("fidget").setup()
+
+local installable_servers = servers.installable
+if helpers.isModuleAvailable("stripe") then
+  installable_servers = {}
+end
 
 -- Installer config
 require("mason").setup({
@@ -13,7 +19,7 @@ require("mason").setup({
   },
 })
 require("mason-lspconfig").setup({
-  ensure_installed = servers.installable,
+  ensure_installed = installable_servers,
 })
 require("mason-tool-installer").setup({
   ensure_installed = {
