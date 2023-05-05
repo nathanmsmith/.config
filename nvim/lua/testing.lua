e0279ef50565966e2249a2f0d36a74c00a1d252b
Nathan Smith <nms@stripe.com>
2023-05-05 14:37:45 -0700

    Add back test tab name

--- nvim/lua/testing.lua
+++ nvim/lua/testing.lua
@@ -9,7 +9,7 @@ vim.g["test#custom_strategies"] = {
 }
 
 vim.g["VimuxRunnerType"] = "window"
--- vim.g["VimuxRunnerName"] = "tests"
+vim.g["VimuxRunnerName"] = "tests"
 vim.g["test#preserve_screen"] = 1
 
 if os.getenv("TMUX") then
