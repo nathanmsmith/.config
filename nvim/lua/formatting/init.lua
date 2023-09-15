c519c2c7e1b39c497c03436a8713425c717522c7
Nathan Smith <nms@stripe.com>
2023-09-15 08:28:59 -0700

    Add commands to enable/disable formatting
    https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save

--- nvim/lua/formatting/init.lua
+++ nvim/lua/formatting/init.lua
@@ -28,3 +28,23 @@ else
     },
   })
 end
+
+-- Commands to enable/disable formatting
+-- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
