4183d76ef9fa92de5b61523d039086b398e0ac48
Nathan Smith <nathan@hotseat.io>
2023-10-10 22:43:28 -0700

    Add m4b-tool alias

--- fish/functions/ibrew.fish
+++
@@ -1,3 +0,0 @@
-function ibrew --wraps='arch -x86_64 brew' --description 'alias for intel homebrew'
-    arch -x86_64 /usr/local/bin/brew $argv
-end
---
+++ fish/functions/m4b-tool.fish
@@ -0,0 +1,3 @@
+function m4b-tool
+    docker run -it --rm -u (id -u):(id -g) -v "$PWD":/mnt sandreas/m4b-tool:latest
+end
