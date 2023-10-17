bdd49cbb32fb3868b52d48f050f7e3411d462778
Nathan Smith <nms@stripe.com>
2023-10-17 13:46:56 -0700

    trouble.nvim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -254,7 +254,20 @@ return {
   {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true, icons = false },
+    opts = {
+      icons = false,
+      fold_open = "v", -- icon used for open folds
+      fold_closed = ">", -- icon used for closed folds
+      indent_lines = false, -- add an indent guide below the fold icons
+      signs = {
+        -- icons / text used for a diagnostic
+        error = "error",
+        warning = "warn",
+        hint = "hint",
+        information = "info",
+      },
+      use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+    },
     keys = {
       { "<leader>xx", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
       { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
