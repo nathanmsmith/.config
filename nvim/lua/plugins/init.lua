8542d900e5c2f0a293d156280d5190193c6c3c13
Nathan Smith <nms@stripe.com>
2024-01-18 14:56:42 -0800

    wip

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -161,6 +161,32 @@ return {
       end, { desc = "Edit Snippet files" })
     end,
   },
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = "nvim-telescope/telescope.nvim",
+    opts = {
+      snippetDir = vim.fn.stdpath("config") .. "/snippets",
+      -- editSnippetPopup = {
+      --   height = 0.4, -- relative to the window, number between 0 and 1
+      --   width = 0.6,
+      --   border = "rounded",
+      --   keymaps = {
+      --     cancel = "q",
+      --     saveChanges = "<CR>", -- alternatively, can also use `:w`
+      --     goBackToSearch = "<BS>",
+      --     deleteSnippet = "<C-BS>",
+      --     duplicateSnippet = "<C-d>",
+      --     openInFile = "<C-o>",
+      --     insertNextToken = "<C-t>", -- insert & normal mode
+      --     jumpBetweenBodyAndPrefix = "<C-Tab>", -- insert & normal mode
+      --   },
+      -- },
+      telescope = {
+        alsoSearchSnippetBody = true,
+      },
+      jsonFormatter = "jq",
+    },
+  },
 
   -- Autopair
   {
