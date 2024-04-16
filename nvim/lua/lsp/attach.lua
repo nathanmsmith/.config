ad0e3e99120c0806a040237ad03671f618f1fcf1
Nathan Smith <nms@stripe.com>
2024-04-16 09:31:23 -0700

    Disable semantic tokens from LSP

--- nvim/lua/lsp/attach.lua
+++ nvim/lua/lsp/attach.lua
@@ -32,22 +32,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, "[W]orkspace [L]ist Folders")
 
+    -- Disable semantic tokens, which use a lot of memory
+    -- https://github.com/pmizio/typescript-tools.nvim/issues/228#issuecomment-1903604947
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+
     -- The following two autocommands are used to highlight references of the
     -- word under your cursor when your cursor rests there for a little while.
     --    See `:help CursorHold` for information about when this is executed
     --
     -- When you move your cursor, the highlights will be cleared (the second autocommand).
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = event.buf,
-        callback = vim.lsp.buf.document_highlight,
-      })
+    -- local client = vim.lsp.get_client_by_id(event.data.client_id)
+    -- if client and client.server_capabilities.documentHighlightProvider then
+    --   vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    --     buffer = event.buf,
+    --     callback = vim.lsp.buf.document_highlight,
+    --   })
 
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = event.buf,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
+    --   vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+    --     buffer = event.buf,
+    --     callback = vim.lsp.buf.clear_references,
+    --   })
+    -- end
   end,
 })
