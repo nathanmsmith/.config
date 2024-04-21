b027a2a52d58c8908598a8956b4391d4fea4495d
Nathan Smith <nathan@hotseat.io>
2024-04-20 20:50:31 -0700

    Update git signs

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -18,13 +18,6 @@ return {
     "lewis6991/gitsigns.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
       on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
