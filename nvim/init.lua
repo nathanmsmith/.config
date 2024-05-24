d593ff897ab24a16943fa009d33cead6da75f687
Nathan Smith <nms@stripe.com>
2024-05-24 10:03:21 -0700

    Disable clipboard stuff for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -72,16 +72,16 @@ vim.cmd([[
   :command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1)
 ]])
 
-if vim.env.TMUX then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("tmux_osc52").copy("+"),
-      ["*"] = require("tmux_osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("tmux_osc52").paste("+"),
-      ["*"] = require("tmux_osc52").paste("*"),
-    },
-  }
-end
+-- if vim.env.TMUX then
+--   vim.g.clipboard = {
+--     name = "OSC 52",
+--     copy = {
+--       ["+"] = require("tmux_osc52").copy("+"),
+--       ["*"] = require("tmux_osc52").copy("*"),
+--     },
+--     paste = {
+--       ["+"] = require("tmux_osc52").paste("+"),
+--       ["*"] = require("tmux_osc52").paste("*"),
+--     },
+--   }
+-- end
