620f43d9815519520827db83c6c134716cc74aa3
Nathan Smith <nathan@hotseat.io>
2024-05-25 10:04:40 -0700

    Add inlay hints toggle

--- nvim/lua/lsp/attach.lua
+++ nvim/lua/lsp/attach.lua
@@ -23,6 +23,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
     nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    nmap("<leader>ih", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    end, "Toggle [I]nlay [H]ints")
 
     -- Lesser used LSP functionality
     nmap("<leader>D", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
