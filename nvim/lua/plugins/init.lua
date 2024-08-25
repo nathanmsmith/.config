2a1cc9863ab982ac9e2646ecfac76a9c606b0aaf
Nathan Smith <nathan@hotseat.io>
2024-08-24 20:27:16 -0700

    Fix which-key mappings

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -51,7 +51,41 @@ return {
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 200
-      require("which-key").setup({})
+      require("which-key").setup({
+        icons = {
+          mappings = false,
+          keys = {
+            Up = "<Up> ",
+            Down = "<Down> ",
+            Left = "<Left> ",
+            Right = "<Right> ",
+            C = "<C-…> ",
+            M = "<M-…> ",
+            D = "<D-…> ",
+            S = "<S-…> ",
+            CR = "<CR> ",
+            Esc = "<Esc> ",
+            ScrollWheelDown = "<ScrollWheelDown> ",
+            ScrollWheelUp = "<ScrollWheelUp> ",
+            NL = "<NL> ",
+            BS = "<BS> ",
+            Space = "<Space> ",
+            Tab = "<Tab> ",
+            F1 = "<F1>",
+            F2 = "<F2>",
+            F3 = "<F3>",
+            F4 = "<F4>",
+            F5 = "<F5>",
+            F6 = "<F6>",
+            F7 = "<F7>",
+            F8 = "<F8>",
+            F9 = "<F9>",
+            F10 = "<F10>",
+            F11 = "<F11>",
+            F12 = "<F12>",
+          },
+        },
+      })
     end,
   },
 
