b5d3556dd96375d54136089cc7295681eb240a8e
Nathan Smith <nathan@nathansmith.io>
2024-09-29 15:30:24 -0700

    Only use rbenv and nodenv if installed

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,10 @@ if status --is-login; and status --is-interactive
     if test -e ~/.config/fish/stripe-config.fish
         source ~/.config/fish/stripe-config.fish
     end
-    if not test -e ~/.config/fish/conf.d/stripe-abbrs-devbox.fish
+    if command -v rbenv >/dev/null 2>&1
         source (rbenv init -|psub)
+    end
+    if command -v nodenv >/dev/null 2>&1
         source (nodenv init -|psub)
     end
     if test -e {$HOME}/.iterm2_shell_integration.fish
