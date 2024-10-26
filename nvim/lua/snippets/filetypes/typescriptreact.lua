a5f910f897a009394b4fc63252c28e962ee782b3
Nathan Smith <nathan@nathansmith.io>
2024-10-26 10:58:15 -0700

    Remove old lua snippets

--- nvim/lua/snippets/filetypes/typescriptreact.lua
+++
@@ -1,4 +0,0 @@
-local typescript_snippets, typescript_autosnippets = require("snippets.filetypes.typescript")
-local snippets, autosnippets = vim.deepcopy(typescript_snippets), vim.deepcopy(typescript_autosnippets)
-
-return snippets, autosnippets
