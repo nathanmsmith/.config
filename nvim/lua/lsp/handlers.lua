d48c662f5a9257e9968c3b5ea6646f852e5d2508
Nathan Smith <nathan@nathansmith.io>
2024-10-26 22:09:53 -0700

    Allow jsonc trailing commas

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -5,3 +5,11 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "single",
 })
+
+vim.lsp.handlers["workspace/diagnostic/refresh"] = function(err, result, ctx, config)
+  vim.notify("refresh")
+  local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.diagnostic.reset(ns, bufnr)
+  return true
+end
--- nvim/lua/lsp/jsonls.lua
+++ nvim/lua/lsp/jsonls.lua
@@ -1,7 +1,6 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-print("nathan jsonls loaded")
 require("lspconfig").jsonls.setup({
   capabilities = capabilities,
   init_options = {
@@ -9,28 +8,17 @@ require("lspconfig").jsonls.setup({
   },
   settings = {
     json = {
-      schemas = require("schemastore").json.schemas(),
-      validate = { enable = true },
+      schemas = vim.tbl_extend("keep", {
+        {
+          fileMatch = { "*.jsonc" },
+          schema = {
+            allowTrailingCommas = true,
+          },
+        },
+      }, require("schemastore").json.schemas()),
+      validate = {
+        enable = true,
+      },
     },
-  },
-  handlers = {
-    ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-      print(vim.inspect(err))
-      print(vim.inspect(result))
-      print(vim.inspect(ctx))
-      print(vim.inspect(config))
-      -- if string.match(result.uri, "%.json5$", -6) and result.diagnostics ~= nil then
-      --   local idx = 1
-      --   while idx <= #result.diagnostics do
-      --     -- "Comments are not permitted in JSON."
-      --     if result.diagnostics[idx].code == 521 then
-      --       table.remove(result.diagnostics, idx)
-      --     else
-      --       idx = idx + 1
-      --     end
-      --   end
-      -- end
-      vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx, config)
-    end,
   },
 })
