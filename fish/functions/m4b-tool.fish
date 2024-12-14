6eb7a16e981ca207e79c2421ad0c95743be7b7aa
Nathan Smith <nathan@nathansmith.io>
2024-12-14 08:44:24 -0800

    Accept args for m4b-tool

--- fish/functions/m4b-tool.fish
+++ fish/functions/m4b-tool.fish
@@ -1,3 +1,3 @@
 function m4b-tool
-    docker run -it --rm -u (id -u):(id -g) -v "$PWD":/mnt sandreas/m4b-tool:latest
+    docker run -it --rm -u (id -u):(id -g) -v "$PWD":/mnt sandreas/m4b-tool:latest $argv
 end
