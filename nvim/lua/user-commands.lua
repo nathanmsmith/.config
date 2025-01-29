5ed391281ee86b9d5a625627ca2ab90b28eab6db
Nathan Smith <nathan@nathansmith.io>
2025-01-28 22:47:06 -0800

    Fix user commands

--- nvim/lua/user-commands.lua
+++ nvim/lua/user-commands.lua
@@ -1,6 +1,15 @@
-vim.api.nvim_create_user_command("Finder", '!open "%:p:h"', { bang = true })
+vim.api.nvim_create_user_command("Finder", function()
+  -- TODO
+  -- if filetype == oil then
+  --   process
+  --   end
+  if vim.fn.has("mac") ~= 1 then
+    vim.notify("This command only works on macOS", vim.log.levels.ERROR)
+    return
+  end
+  vim.fn.system("open " .. vim.fn.expand("%:p"))
+end, { bang = true })
 vim.api.nvim_create_user_command("FileName", function()
-  -- vim.b.
-  -- TODO: get file name in Lua ("%")
-  -- vim.fn.setreg("*", "@%")
+  local name_and_path = vim.fn.expand("%")
+  vim.fn.setreg("+", name_and_path)
 end, { bang = true })
