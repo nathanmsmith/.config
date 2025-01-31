73647041a7742babd96d2893ba72ba079c88a0d2
Nathan Smith <nathan@nathansmith.io>
2025-01-30 23:47:03 -0800

    Add a scratchpad todo

--- nvim/lua/scratchpad.lua
+++ nvim/lua/scratchpad.lua
@@ -105,3 +105,8 @@ vim.keymap.set("n", "<Leader>nt", ":tabnew<CR>", {
   noremap = true,
   desc = "Create new tab",
 })
+
+-- TODO: customize hovers
+-- see
+-- 1. https://gist.github.com/OXY2DEV/645c90df32095a8a397735d0be646452
+-- 2. https://github.com/neovim/neovim/pull/30935#issuecomment-2449897286
