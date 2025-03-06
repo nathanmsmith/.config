e1be7d7026a1e3513cfdf2ecb5eac5eedfab9010
Nathan Smith <nathan@nathansmith.io>
2025-03-05 20:18:08 -0800

    Add dnd command

---
+++ fish/functions/dnd.fish
@@ -0,0 +1,3 @@
+function dnd --wraps="cd '/Users/nathan/Documents/Dungeons and Dragons/Campaigns/Lost Mine of Phandelver 2025'" --description 'Go to active D&D Campaign'
+    cd "/Users/nathan/Documents/Dungeons and Dragons/Campaigns/Lost Mine of Phandelver 2025"
+end
