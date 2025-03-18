dcc4b345f2359b81705d49775905f3420d6cf3d6
Nathan Smith <nms@stripe.com>
2025-03-17 22:26:04 -0700

    WIP: first pass at a marks plugin

--- nvim/lua/marks.lua
+++ nvim/lua/marks.lua
@@ -1,52 +1,182 @@
--- vim.keymap.del("n", "m")
---
--- -- Add global mark
--- vim.keymap.set("n", "ma", function()
---   -- get global marks
--- end, { desc = "[M]ark [A]dd" })
---
--- -- Add local mark
--- vim.keymap.set("n", "mA", function()
---   -- get global marks
--- end, { desc = "[M]ark [A]dd" })
---
--- -- internal.marks = function(opts)
--- --   local local_marks = {
--- --     items = vim.fn.getmarklist(opts.bufnr),
--- --     name_func = function(_, line)
--- --       return vim.api.nvim_buf_get_lines(opts.bufnr, line - 1, line, false)[1]
--- --     end,
--- --   }
--- --   local global_marks = {
--- --     items = vim.fn.getmarklist(),
--- --     name_func = function(mark, _)
--- --       -- get buffer name if it is opened, otherwise get file name
--- --       return vim.api.nvim_get_mark(mark, {})[4]
--- --     end,
--- --   }
--- --   local marks_table = {}
--- --   local marks_others = {}
--- --   local bufname = vim.api.nvim_buf_get_name(opts.bufnr)
--- --   for _, cnf in ipairs { local_marks, global_marks } do
--- --     for _, v in ipairs(cnf.items) do
--- --       -- strip the first single quote character
--- --       local mark = string.sub(v.mark, 2, 3)
--- --       local _, lnum, col, _ = unpack(v.pos)
--- --       local name = cnf.name_func(mark, lnum)
--- --       -- same format to :marks command
--- --       local line = string.format("%s %6d %4d %s", mark, lnum, col - 1, name)
--- --       local row = {
--- --         line = line,
--- --         lnum = lnum,
--- --         col = col,
--- --         filename = utils.path_expand(v.file or bufname),
--- --       }
--- --       -- non alphanumeric marks goes to last
--- --       if mark:match "%w" then
--- --         table.insert(marks_table, row)
--- --       else
--- --         table.insert(marks_others, row)
--- --       end
--- --     end
--- --   end
--- --   marks_table = vim.fn.extend(marks_table, marks_others)
+------ MARKS
+-- Core ideas:
+-- `ma` -> Add a global mark (Start with A and pick first unused mark)
+-- `mnX` -> Add a named mark X
+-- `mrX` -> Replace the current mark with mark X
+
+-- TODO: `mA` -> Add a local mark (Start with a and pick first unused mark)
+-- TODO: `mdc` -> Delete mark on current line
+-- `mda` -> Delete all marks globally
+-- `mdA` -> Delete all marks locally
+-- `ml` -> open list of marks
+
+local M = {}
+
+M.config = {
+  signs = {
+    enabled = true,
+    priority = 100,
+    show_uppercase = true,
+    show_lowercase = true,
+    show_numbered = true,
+  },
+  sign_group = "MarkGutter",
+  sign_name_prefix = "MarkGutter_",
+}
+
+-- Initialize the plugin
+function M.setup(user_config)
+  -- Merge user config with defaults
+  if user_config then
+    M.config = vim.tbl_deep_extend("force", M.config, user_config)
+  end
+
+  -- Define highlight groups
+  -- TODO: adjust these
+  vim.cmd([[
+    highlight default link MarkGutterLower Number
+    highlight default link MarkGutterUpper Number
+    highlight default link MarkGutterNumbered Number
+  ]])
+
+  -- Create signs for lowercase, uppercase, and numbered marks
+  if M.config.signs.enabled then
+    -- Lowercase marks (a-z)
+    if M.config.signs.show_lowercase then
+      for i = 97, 122 do -- ASCII codes for a-z
+        local char = string.char(i)
+        vim.fn.sign_define(M.config.sign_name_prefix .. char, {
+          text = char,
+          texthl = "MarkGutterLower",
+          linehl = "",
+          numhl = "",
+        })
+      end
+    end
+
+    -- Uppercase marks (A-Z)
+    if M.config.signs.show_uppercase then
+      for i = 65, 90 do -- ASCII codes for A-Z
+        local char = string.char(i)
+        vim.fn.sign_define(M.config.sign_name_prefix .. char, {
+          text = char,
+          texthl = "MarkGutterUpper",
+          linehl = "",
+          numhl = "",
+        })
+      end
+    end
+
+    -- Numbered marks (0-9)
+    if M.config.signs.show_numbered then
+      for i = 48, 57 do -- ASCII codes for 0-9
+        local char = string.char(i)
+        vim.fn.sign_define(M.config.sign_name_prefix .. char, {
+          text = char,
+          texthl = "MarkGutterNumbered",
+          linehl = "",
+          numhl = "",
+        })
+      end
+    end
+  end
+
+  -- Set up autocommands to update the signs
+  local augroup = vim.api.nvim_create_augroup("MarkGutter", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave", "CmdlineLeave" }, {
+    group = augroup,
+    callback = function()
+      M.update_marks()
+    end,
+  })
+
+  -- Mark commands can change marks
+  vim.api.nvim_create_autocmd("CmdlineLeave", {
+    group = augroup,
+    pattern = "*",
+    callback = function()
+      local cmd = vim.fn.getcmdline()
+      if cmd:match("^%s*m%s*[a-zA-Z0-9]") then
+        vim.schedule(function()
+          M.update_marks()
+        end)
+      end
+    end,
+  })
+end
+
+-- Find next unused global mark (A-Z)
+function M.find_next_unused_global_mark()
+  local marks = vim.fn.getmarklist()
+  local used_marks = {}
+
+  for _, mark in ipairs(marks) do
+    local mark_char = mark.mark:sub(2, 2)
+    if mark_char:match("[A-Z]") then
+      used_marks[mark_char] = true
+    end
+  end
+
+  for i = 65, 90 do -- ASCII codes for A-Z
+    local char = string.char(i)
+    if not used_marks[char] then
+      return char
+    end
+  end
+
+  return "A" -- If all marks are used, default to A
+end
+
+-- Rerender all marks in the sign column
+function M.update_marks()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- Clear all existing marks
+  vim.fn.sign_unplace(M.config.sign_group, { buffer = bufnr })
+
+  -- Get all marks
+  local local_marks = vim.fn.getmarklist(bufnr)
+  local global_marks = vim.fn.getmarklist()
+
+  local all_marks = vim.list_extend(local_marks, global_marks)
+
+  -- Place signs for each mark
+  for _, mark in ipairs(all_marks) do
+    -- Extract the character from 'x
+    local mark_char = mark.mark:sub(2, 2)
+
+    local should_show = false
+    if M.config.signs.show_lowercase and mark_char:match("[a-z]") then
+      should_show = true
+    elseif M.config.signs.show_uppercase and mark_char:match("[A-Z]") then
+      should_show = true
+    elseif M.config.signs.show_numbered and mark_char:match("[0-9]") then
+      should_show = true
+    end
+
+    if should_show then
+      vim.fn.sign_place(0, M.config.sign_group, M.config.sign_name_prefix .. mark_char, bufnr, {
+        lnum = mark.pos[2],
+        priority = M.config.signs.priority,
+      })
+    end
+  end
+end
+
+-- Core ideas:
+vim.keymap.set("n", "ma", function()
+  local mark = M.find_next_unused_global_mark()
+  vim.cmd("normal! m" .. mark)
+  M.update_marks()
+end, { desc = "Add a mark" })
+vim.keymap.set("n", "mda", function()
+  vim.cmd("delmarks A-Z")
+  M.update_marks()
+end, { desc = "Delete all global marks" })
+
+vim.keymap.set("n", "ml", require("telescope.builtin").marks, { desc = "List marks" })
+
+M.setup()
+
+return M
