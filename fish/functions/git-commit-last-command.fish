649d4b239893e334fe00b0825b0820be36ca24eb
Nathan Smith <nathan@nathansmith.io>
2025-03-23 23:54:55 -0700

    Go back one more for the last command if necessary

--- fish/functions/git-commit-last-command.fish
+++ fish/functions/git-commit-last-command.fish
@@ -5,6 +5,12 @@
 # --> "git add ."
 function git-commit-last-command --wraps="git commit"
   set last_command (history | head -1)
+  if test "$last_command" = "git commit-last-command" -o "$last_command" = "git-commit-last-command"
+    # Go back one more
+    set last_command (history | head -2 | tail -1)
+  end
+
+
   git add .
   git commit -m "$last_command" $argv
 end
