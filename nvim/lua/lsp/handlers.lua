9a785bf02a8d340ff4748b63d011e62b312ddaa9
Nathan Smith <nathan@nathansmith.io>
2025-04-04 23:42:44 -0700

    Fix borders with nvim 0.11

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -1,10 +1,24 @@
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "single",
-})
+local hover = vim.lsp.buf.hover
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.hover = function()
+  return hover({
+    border = "single",
+    -- max_width = 100,
+    max_width = math.floor(vim.o.columns * 0.7),
+    max_height = math.floor(vim.o.lines * 0.7),
+  })
+end
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "single",
-})
+local signature_help = vim.lsp.buf.signature_help
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.signature_help = function()
+  return signature_help({
+    border = "single",
+    -- max_width = 100,
+    max_width = math.floor(vim.o.columns * 0.7),
+    max_height = math.floor(vim.o.lines * 0.7),
+  })
+end
 
 vim.lsp.handlers["workspace/diagnostic/refresh"] = function(err, result, ctx, config)
   vim.notify("refresh")
--- nvim/lua/scratchpad.lua
+++ nvim/lua/scratchpad.lua
@@ -78,3 +78,12 @@ vim.api.nvim_create_user_command(
 --   lnum = 174,
 --   priority = 10,
 -- })
+--
+--
+vim.api.nvim_create_user_command("ReloadConfig", function()
+  -- Source the init.lua file
+  dofile(vim.env.MYVIMRC)
+
+  -- Notify the user
+  vim.notify("Neovim configuration reloaded!", vim.log.levels.INFO)
+end, {})
