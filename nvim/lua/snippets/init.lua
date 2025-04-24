cafe8cb63109074e28b2ef0781a215595ed56f12
Nathan Smith <nms@stripe.com>
2025-04-24 11:14:24 -0700

    Add todo: lsp-aware snippets

--- nvim/lua/snippets/init.lua
+++ nvim/lua/snippets/init.lua
@@ -60,6 +60,57 @@ vim.keymap.set({ "i", "s" }, "<C-h>", function()
   end
 end, { silent = true })
 
+-- TODO: LSP-aware version
+-- Helper to check if LSP snippet can be expanded
+-- local function lsp_expandable()
+--   local lsp_snippet = vim.lsp.util.get_current_line():match("^%s*(%S+)")
+--   if not lsp_snippet then return false end
+--
+--   -- Check if there's an LSP snippet for the word under cursor
+--   for _, client in ipairs(vim.lsp.get_active_clients()) do
+--     if client.server_capabilities.completionProvider and
+--        client.server_capabilities.completionProvider.resolveProvider then
+--       return true
+--     end
+--   end
+--   return false
+-- end
+--
+-- local active = function(filter)
+--   filter = filter or {}
+--   filter.direction = filter.direction or 1
+--
+--   if filter.direction == 1 then
+--     return ls.expand_or_jumpable() or lsp_expandable()
+--   else
+--     return ls.jumpable(filter.direction)
+--   end
+-- end
+--
+-- local jump = function(direction)
+--   if direction == 1 then
+--     if ls.expandable() then
+--       return ls.expand()
+--     elseif lsp_expandable() then
+--       -- This expands LSP snippets through LuaSnip's interface
+--       local bufnr = vim.api.nvim_get_current_buf()
+--       local pos = vim.api.nvim_win_get_cursor(0)
+--       local col = pos[2]
+--       local line = vim.api.nvim_get_current_line()
+--       local word = line:match("^%s*(%S+)")
+--
+--       if word then
+--         -- Tell LuaSnip to expand LSP snippet
+--         return ls.lsp_expand(word)
+--       end
+--     elseif ls.jumpable(1) then
+--       return ls.jump(1)
+--     end
+--   else
+--     return ls.jumpable(-1) and ls.jump(-1)
+--   end
+-- end
+
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = { vim.fn.stdpath("config") .. "/snippets", "~/stripe/stripe-config/nvim/snippets" },
 })
