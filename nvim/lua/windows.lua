79feca4814c8577295b0a8008796d4f4c253d716
Nathan Smith <nathan@nathansmith.io>
2025-07-04 10:06:21 -0700

    Remove an old todo

--- nvim/lua/windows.lua
+++ nvim/lua/windows.lua
@@ -9,13 +9,9 @@ vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
 vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
 vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
 vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
-
 vim.g.maximizer_set_default_mapping = false
 vim.keymap.set("n", "<C-W>m", "<Cmd>MaximizerToggle<CR>", { silent = true })
--- TODO: enable this line after I figure out more about libtermkey stuff
--- https://neovim.io/news/2022/04#distinguishing-modifier-keys
--- https://sw.kovidgoyal.net/kitty/keyboard-protocol/
--- vim.keymap.set('n', '<C-M>', '<Cmd>MaximizerToggle<CR>', {silent = true})
+vim.keymap.set("n", "<C-M>", "<Cmd>MaximizerToggle<CR>", { silent = true })
 
 -- Navigate around tabs easier.
 -- This overrides vim's default ctags navigation which is okay because who uses ctags anymore?
@@ -23,3 +19,7 @@ vim.keymap.set("n", "[t", ":tabprevious<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "]t", ":tabnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "[T", ":tabfirst<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "]T", ":tablast<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>nt", ":tabnew<CR>", {
+  noremap = true,
+  desc = "Create new tab",
+})
