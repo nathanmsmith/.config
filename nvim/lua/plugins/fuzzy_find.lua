99998eb2af3503c932b7b86045acea6526dc8123
Nathan Smith <nathan@nathansmith.io>
2025-09-20 17:10:26 -0700

    ctrl-q to send all results to quickfix list

--- nvim/lua/plugins/fuzzy_find.lua
+++ nvim/lua/plugins/fuzzy_find.lua
@@ -1,6 +1,17 @@
 return {
   {
     "ibhagwan/fzf-lua",
-    opts = {},
+    config = function()
+      local actions = require("fzf-lua").actions
+      require("fzf-lua").setup({
+        actions = {
+          true,
+          files = {
+            true,
+            ["ctrl-q"] = { fn = actions.file_sel_to_qf, prefix = "select-all" },
+          },
+        },
+      })
+    end,
   },
 }
