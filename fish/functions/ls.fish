29786674b763520f31edf596f75d115f71b707de
Nathan Smith <nathan@nathansmith.io>
2025-11-29 16:22:48 -0800

    Create ls fallback

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
 function ls --wraps=eza --description 'alias ls eza'
-    eza --time-style '+%Y-%m-%d %I:%M%P' $argv
+    if command -q eza
+        eza --time-style '+%Y-%m-%d %I:%M%P' $argv
+    else
+        command ls $argv
+    end
 end
