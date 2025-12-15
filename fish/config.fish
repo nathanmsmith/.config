f8abcc3a0a4e81424cd27bdab5d947c0edfa9d1b
Nathan Smith <nathan@nathansmith.io>
2025-12-15 11:01:10 -1000

    Add asdf

--- fish/config.fish
+++ fish/config.fish
@@ -26,4 +26,18 @@ if status --is-login; and status --is-interactive
   # TODO: look into `prompt_pwd`
   # TODO: look into `fish_vcs_prompt`
 
+  # ASDF configuration code
+  if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+  else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+  end
+
+  # Do not use fish_add_path (added in Fish 3.2) because it
+  # potentially changes the order of items in PATH
+  if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+  end
+  set --erase _asdf_shims
+
 end
