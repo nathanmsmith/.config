8598c56a75644f5e2baa691c1102bc36b250a0c6
Nathan Smith <nathan@nathansmith.io>
2026-01-17 12:19:19 -0800

    Improve git prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -15,27 +15,29 @@ function _directory -d "Writes the colorized present working directory"
 end
 
 function _git_status -d "Writes the current git status, if there is one."
-    set -l branch_name (__fish_git_prompt "%s" | sed 's/ //')
-    if test -z $branch_name
+    # Check if we're in a git repo
+    set -l branch_name (__fish_git_prompt "%s" 2>/dev/null)
+    if test -z "$branch_name"
         return
     end
 
     echo -s (set_color normal) " on "
+
+    # Check dirty state manually for color decision
     set -l dirty (command git status --porcelain 2>/dev/null)
     if test -z "$dirty"
         set_color --bold green
     else
         set_color --bold red
     end
-    echo -s "$branch_name" (set_color normal)
+    echo -s (string trim $branch_name) (set_color normal)
 
-    set -l unpushed_changes (command git cherry -v 2>/dev/null)
-    if test -z "$unpushed_changes"
-        return
+    # Check for unpushed commits using optimized git command
+    # Use git rev-list --count which is faster than git cherry
+    set -l unpushed (command git rev-list --count @{upstream}..HEAD 2>/dev/null)
+    if test -n "$unpushed" -a "$unpushed" -gt 0
+        echo -s " with " (set_color --bold magenta) unpushed (set_color normal)
     end
-
-    echo -s " with " (set_color --bold magenta) unpushed (set_color normal)
-
 end
 
 function fish_prompt -d "Write out the prompt"
