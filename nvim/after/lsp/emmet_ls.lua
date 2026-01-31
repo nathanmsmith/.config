94643d0140550421009671cc1e2e5f7e85583140
Nathan Smith <nathan@nathansmith.io>
2026-01-30 17:54:43 -0800

    Enable emmetlsp

---
+++ nvim/after/lsp/emmet_ls.lua
@@ -0,0 +1,17 @@
+-- Emmet LSP configuration for HTML, CSS, and JSX/TSX
+return {
+  filetypes = {
+    "html",
+    "css",
+    "scss",
+    "sass",
+    "less",
+    "javascriptreact",
+    "typescriptreact",
+  },
+  init_options = {
+    showExpandedAbbreviation = "always",
+    showSuggestionsAsSnippets = true,
+    preferences = {},
+  },
+}
