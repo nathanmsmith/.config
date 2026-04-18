39f69b2c5e05a864c67341f928c85ee8b8a53991
Nathan Smith <nathan@nathansmith.io>
2026-04-18 14:38:22 -0700

    Disable tailwind colors

---
+++ nvim/after/lsp/tailwindcss.lua
@@ -0,0 +1,5 @@
+return {
+  settings = {
+    tailwindCSS = { colorDecorators = false },
+  },
+}
--- nvim/plugin/nvim-highlight-colors.lua
+++ nvim/plugin/nvim-highlight-colors.lua
@@ -2,4 +2,4 @@ vim.pack.add({
   "https://github.com/brenoprata10/nvim-highlight-colors",
 })
 
-require("nvim-highlight-colors").setup({ render = "virtual", enable_tailwind = true })
+require("nvim-highlight-colors").setup({ render = "virtual", enable_tailwind = false })
